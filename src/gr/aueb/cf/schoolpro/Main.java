2c0ea7b5280d930d3d2d35c6a77c2107548b0f8a
Rmoney <DionisisLiontos@outlook.com>
2024-01-30 19:13:57 +0200

    Main class, Loginform

---
+++ src/gr/aueb/cf/schoolpro/Main.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.schoolpro;
+
+import java.awt.EventQueue;
+
+public class Main {
+	private static Login loginForm;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
